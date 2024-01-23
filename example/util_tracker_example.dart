337e63d44b50e289fcc028bf88457623e276e168
Tech Chain <techchain@TechdeMac-mini.local>
2024-01-23 18:08:01 +0800

    util init

---
+++ example/util_tracker_example.dart
@@ -0,0 +1,6 @@
+import 'package:util_tracker/util_tracker.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/util_tracker_test.dart
@@ -0,0 +1,16 @@
+import 'package:util_tracker/util_tracker.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
